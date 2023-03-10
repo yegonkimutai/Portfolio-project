b3e4fe3a09aa5148ded99abe4d23214f7f558edf
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-03-10 18:42:00 +0300

    Modify content

--- validation.js
+++ validation.js
@@ -2,20 +2,3 @@ const form = document.getElementById('contact-form');
 const email = document.getElementById('email');
 const error = document.getElementById('error-msg');
 
-form.addEventListener('submit', (event) => {
-  const errArray = [];
-
-  if (email.value === '') {
-    errArray.push('Invalid email');
-  } else if (email.value !== email.value.toLowerCase()) {
-    errArray.push('Invalid email, ensure charecters are in lowerCase');
-  } else {
-    form.submit();
-    form.reset();
-  }
-
-  if (errArray.length > 0) {
-    event.preventDefault();
-    error.innerHTML = errArray.join(', ');
-  }
-});
